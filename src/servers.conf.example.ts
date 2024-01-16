5b7aa2d33cdba7410b297d155989f4224a95b952
xray-robot <robot@xray.app>
2024-01-16 14:30:00 +0300

    Removing ports to match Output port mapper over port 80

--- src/servers.conf.example.ts
+++ src/servers.conf.example.ts
@@ -2,9 +2,9 @@ import type { ServerConfig } from "./types"
 
 const ServerConfig: ServerConfig = {
   // IP addresses are forbidden by CF, expose them through CF DNS. HAProxy routes to the desired service on port 80 using the HostResolver header
-  mainnet: ["http://relay-server-1.com", "http://relay-server-2.com"],
-  preview: ["http://relay-server-preview.com"],
-  preprod: ["http://relay-server-preprod.com"],
+  mainnet: ["http://ogmios-server-1.com", "http://ogmios-server-2.com"],
+  preview: ["http://ogmios-server-preview.com"],
+  preprod: ["http://ogmios-server-preprod.com"],
 }
 
 export default ServerConfig
